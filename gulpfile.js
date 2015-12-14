1e62ca03548e58e2637300160992e332d6d5c6d4
Mehdy Dara <mdara@eleven-labs.com>
2015-12-14 17:17:05 +0100

    Fix Babel ESLint error

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ function istanbulCover() {
 function mochaTest() {
   return gulp.src('test/**/*.js')
     .pipe(mocha({reporter: 'spec'}))
-    .once('error', function errorHandler(err) {
+    .once('error', err => {
       gutil.log(gutil.colors.red('[Mocha]'), err.toString());
       process.exit(1);
     })
